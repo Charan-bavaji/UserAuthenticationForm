b4af516612e549cb9c4228e443e819ddfe3451e4
Charan-bavaji <charan.r7760@gmail.com>
2023-06-23 23:27:51 +0530

    setUp done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path:'/',
+      element: <div> Root Route</div>
+    },
+    {
+      path:'/register',
+      element: <div> Register Route</div>
+    }
+  ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <RouterProvider router={router}></RouterProvider>
+    </main>
   );
 }
 

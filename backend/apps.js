f4776d18d01308cf1a1580b0767355aaad7b1a68
Charan-bavaji <charan.r7760@gmail.com>
2023-06-30 00:12:56 +0530

    login router done

--- backend/apps.js
+++ backend/apps.js
@@ -14,18 +14,18 @@ app.post('/signup', (req, res) => {
   const { email, password } = req.body;
   // Check if the email or password is missing
   if (!email && !password) {
-    res.send("email and password required")
+    return res.send("email and password required")
   }
 
   // Check if the email is already taken
   // const userExist = USERS.map((obj) => obj.email === email);
   const userExists = USERS.some(obj => obj.hasOwnProperty("email") && obj["email"] === email);
   if (userExists) {
-    res.send("User already exist")
+    return res.send("User already exist")
   }
   // Create a new user
   USERS.push({ email: email, password: password });
-  res.send("Created successfully");
+  return res.send("Created successfully");
   // Return a success response
 
 });
@@ -37,9 +37,25 @@ app.post('/post', (req, res) => {
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-app.get('/login', (req, res) => {
-  res.send('i am login')
-})
+
+
+// app.post('/login', (req, res) => {
+//   const { email, password } = req.body;
+
+//   if (!email || !password) {
+//     return res.status(401).json({ message: "Enter email and password" });
+//   }
+
+//   const existingUser = USERS.find(
+//     (obj) => obj.hasOwnProperty("email") && obj.email === email && obj.password === password
+//   );
+
+//   if (existingUser) {
+//     return res.status(200).json({ message: "You are logged in" });
+//   } else {
+//     return res.status(401).json({ message: "Invalid email or password" });
+//   }
+// });
 // app.get('/signin', (req, res) => {
 //   res.send('i am login')
 // })

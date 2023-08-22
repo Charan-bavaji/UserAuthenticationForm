d9038321050a5bd53fa91b1fbc069049f94231b7
Charan-bavaji <charan.r7760@gmail.com>
2023-08-23 01:18:40 +0530

    Sign page Ui done

--- backend/apps.js
+++ backend/apps.js
@@ -3,41 +3,19 @@ const app = express()
 const port = 3001
 app.use(express.json());
 
-const USERS = [{
-  email: "obitoUchiha@gamail.com",
-  password: "1234"
-}
-];
 
-app.post('/signup', (req, res) => {
-
-  const { email, password } = req.body;
-  // Check if the email or password is missing
-  if (!email && !password) {
-    return res.send("email and password required")
-  }
-
-  // Check if the email is already taken
-  // const userExist = USERS.map((obj) => obj.email === email);
-  const userExists = USERS.some(obj => obj.hasOwnProperty("email") && obj["email"] === email);
-  if (userExists) {
-    return res.send("User already exist")
-  }
-  // Create a new user
-  USERS.push({ email: email, password: password });
-  return res.send("Created successfully");
-  // Return a success response
-
-});
-
-app.post('/post', (req, res) => {
-  res.send('POST request to the homepage')
-})
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
+// app.post('/signup', (req, res) => {
+//   const { email, password } = req.body;
+//   if (!email && !password) {
+//     return res.send("email and password required")
+//   }
+//   const userExists = USERS.some(obj => obj.hasOwnProperty("email") && obj["email"] === email);
+//   if (userExists) {
+//     return res.send("User already exist")
+//   }
+//   USERS.push({ email: email, password: password });
+//   return res.send("Created successfully");
+// });
 
 app.post('/login', (req, res) => {
   const { email, password } = req.body;
@@ -56,15 +34,3 @@ app.post('/login', (req, res) => {
     return res.status(401).json({ message: "Invalid email or password" });
   }
 });
-app.get('/signin', (req, res) => {
-  res.send('i am login')
-})
-
-
-
-app.get('/dashboard', (req, res) => {
-  res.send('am posting')
-})
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
--- frontend/src/components/Username.js
+++ frontend/src/components/Username.js
@@ -1,35 +1,44 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import avathar from "../assets/dp.jpg"
-import styles from "../styles/username.css"
+// import { Link } from 'react-router-dom'
+// import avathar from "../assets/dp.jpg"
+import "../styles/username.css"
 
 const Username = () => {
   //  Login Ui
-  
+
   return (
-    <div className='container max-auto'>
-      <div className='flex justify-center items-center h-screen '>
-        <div className={styles.glass}>
-          <div className='title flex flex-col items-center '>
-            <h4 className="text-5xl font-bold ">Charan</h4>
-            <span className='py-4 text-xl w-2/3 text-center text-gray-5'>
-              Explore more by connecting with us</span>
+    <div className='Container'>
+      <div className='wrap'>
+        <div className='login'>
+          <button>Log in</button>
+        </div>
+        <div className='sigHead'>
+          <span>
+            Sign Up
+          </span>
+        </div>
+        <div className='autContainer'>
+          <div>
+            <span>Your Email </span>
+            <input type="email" placeholder='email' />
           </div>
+          <div>
 
-          <form className='py-1'>
-            <div className="profile flex justify-center py-4">
-              <img src='' alt='avathar' />
-            </div>
-
-            <div className="textbox flex flex-col items-center gap-6">
-              <input type='text' placeholder='Username' />
-              <button className={styles.btn} type='submit'> Let's Go</button>
-            </div>
-
-            <div className='text-center py-4'>
-              <span className='text-gray-500'>Not a Member <Link className='text-red-500' to='/register'>Register now</Link></span>
-            </div>
-          </form>
+            <span>Name</span>
+            <input type='text' placeholder='Name' />
+          </div>
+          <div>
+            <span>Password</span>
+            <input type='password' placeholder='Password' />
+          </div>
+        </div>
+        <div className='submit'>
+          <button>Sign up</button>
+          <a href='#'>Or sign up with social account</a>
+        </div>
+        <div className='socialBut'>
+          <button>Facebook</button>
+          <button>Twitter</button>
         </div>
       </div>
     </div>

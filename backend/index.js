39f0e00a177a41802d4a80560aaa17b88c075b7e
Charan-bavaji <charan.r7760@gmail.com>
2023-08-24 17:50:26 +0530

    login server is done

--- backend/index.js
+++ backend/index.js
@@ -6,24 +6,39 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const User = [{
     email: "charan@gmail.com",
-    username:"obito",
+    username: "obito",
     password: "123"
 }];
 
 app.post('/signup', (req, res) => {
-    const { email, password,username } = req.body;
-    if(User.find((x)=>x.email === email)){
-        return res.status(401).json({message:"user already exist"});
+    const { email, password, username } = req.body;
+    if (User.find((x) => x.email === email)) {
+        return res.status(401).json({ message: "user already exist" });
     }
     User.push(
-        {email,
-        username,
-        password}
+        {
+            email,
+            username,
+            password
+        }
     );
-    return res.json({message:"Succfully signed"}).status(201);
+    return res.json({ message: "Succfully signed" }).status(201);
 })
 
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
 
+    if (!email || !password) {
+        return res.json({ message: "enter email or password" }).status(401);
+    }
+    User.find((x) => {
+        if (x.email === email && x.password === password) {
+            return res.json({ message: "loginned" }).status((201));
+        } else {
+            return res.json({ message: "email or password invalied" }).status(401);
+        }
+    });
+})
 
 
 

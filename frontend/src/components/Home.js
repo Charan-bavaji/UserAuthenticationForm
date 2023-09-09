da894a99689aef815096b450e6b0a90e02fe5903
Charan-bavaji <charan.r7760@gmail.com>
2023-09-09 22:23:17 +0530

    Added some new features to home page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,11 +2,14 @@ import React from 'react'
 import "../styles/username.css"
 import { Link } from 'react-router-dom'
 const Home = () => {
+
     return (
-        <div className='home'>
+        <div className='ContainerHome'>
+            <div>
             <button><Link to={"/signup"}>Register</Link></button>
             <button><Link to={"/login"}>Login</Link></button>
+            </div>
         </div>
     )
 }
-export default Home
+export default Home;
